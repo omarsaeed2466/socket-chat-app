0103e5847f61d6491087fa65cb7ecd50ed5883ba
omar saeed <os83575@gmail.com>
2022-01-13 02:07:45 +0200

    project init

---
+++ src/main/java/com/example/socketchatapp/SocketChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.socketchatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketChatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocketChatAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/socketchatapp/SocketChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socketchatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketChatAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
