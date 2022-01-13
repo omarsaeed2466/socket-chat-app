16560b2ab0d40f9f9b117aad8a2fb9016f542cb1
omar saeed <os83575@gmail.com>
2022-01-13 03:59:50 +0200

    create model chat messaage

---
+++ src/main/java/com/example/socketchatapp/model/ChatMessage.java
@@ -0,0 +1,40 @@
+package com.example.socketchatapp.model;
+
+public class ChatMessage {
+    private String message ;
+    private String sender ;
+    private String MessageType ;
+    private  ChatType chatType ;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getMessageType() {
+        return MessageType;
+    }
+
+    public void setMessageType(String messageType) {
+        MessageType = messageType;
+    }
+
+    public ChatType getChatType() {
+        return chatType;
+    }
+
+    public void setChatType(ChatType chatType) {
+        this.chatType = chatType;
+    }
+}
---
+++ src/main/java/com/example/socketchatapp/model/ChatType.java
@@ -0,0 +1,5 @@
+package com.example.socketchatapp.model;
+
+public enum ChatType {
+    JOIN,LEAVE,CHAT
+}
